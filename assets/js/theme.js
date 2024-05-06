e928b954481c4902e2a3467747452d493f399be8
ABDUL-REHMAN <abdulrehman034551@gmail.com>
2024-05-06 21:39:06 +0500

    Add More Functionality

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -14,7 +14,7 @@ const isDark=window.matchMedia("(prefers-color-scheme:dark)").matches;
 if(sessionStorage.getItem("theme")){
     $HTML.dataset.theme = sessionStorage.getItem("theme");
 }else{
-    $HTML.dataset.theme=isDark ? "dark" : "light";
+    $HTML.dataset.theme=isDark ? "light" : "dark";
 }
 
 let /**{Boolean} */ isPressed= true;
