5979ed910c3adf9d213c437380420db0f92e2548
ABDUL-REHMAN <abdulrehman034551@gmail.com>
2024-05-14 17:45:30 +0500

    Recipe Section Functionality Add

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -5,3 +5,47 @@
  */
 
 "use strict";
+
+/**
+ * Import
+ */
+
+import { fetchData } from "./api.js";
+import { $skeletonCard, cardQueries } from "./global.js";
+import { getTime } from "./module.js";
+
+/**
+ * Accordion
+ */
+
+const /**{NodeList} */ $accordions =
+    document.querySelectorAll("[data-accordion]");
+
+/**
+ *
+ * @param {*} $element Accordion node
+ */
+
+const initAccordion = function ($element) {
+  const /**{NodeElement} */ $button = $element.querySelector(
+      "[data-accordion-btn]"
+    );
+  let isExpanded = false;
+
+  $button.addEventListener("click", function () {
+    isExpanded = isExpanded ? false : true;
+    this.setAttribute("aria-expanded", isExpanded);
+  });
+};
+
+for (const $accordion of $accordions) initAccordion($accordion);
+
+
+
+
+/**
+ * Filter bar toggle for mobile screen
+ */
+
+
+const /**{NodeElement} */ $filterBar = document.querySelector("[data-filter-bar]");
