38eab14fdc48e5252f6a1aa68746a59c583f76e9
ABDUL-REHMAN <abdulrehman034551@gmail.com>
2024-05-15 15:16:01 +0500

    Add recipes.js Functionality

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -40,12 +40,242 @@ const initAccordion = function ($element) {
 
 for (const $accordion of $accordions) initAccordion($accordion);
 
-
-
-
 /**
  * Filter bar toggle for mobile screen
  */
 
+const /**{NodeElement} */ $filterBar =
+    document.querySelector("[data-filter-bar]");
+const /**{NodeList} */ $filterTogglers = document.querySelectorAll(
+    "[data-filter-toggler]"
+  );
+const /**{NodeElement} */ $overlay = document.querySelector("[data-overlay");
 
-const /**{NodeElement} */ $filterBar = document.querySelector("[data-filter-bar]");
+addEventOnElements($filterTogglers, "click", function () {
+  $filterBar.classList.toggle("active");
+  $overlay.classList.toggle("active");
+  const bodyOverflow = document.body.style.overflow;
+  document.body.style.overflow =
+    bodyOverflow === "hidden" ? "visible" : "hidden";
+});
+
+/** Filter Submit and Clear */
+
+const /**{NodeElement} */ $filterSubmit = document.querySelector(
+    "[data-filter-submit]"
+  );
+const /**{NodeElement} */ $filterClear = document.querySelector(
+    "[data-filter-Clear]"
+  );
+
+const /**{NodeElement} */ $filterSearch = $filterBar.querySelector(
+    "input[type='search']"
+  );
+
+$filterSubmit.addEventListener("click", function () {
+  const /**{NodeList} */ $filterCheckboxes =
+      $filterBar.querySelectorAll("input:checked");
+  const /**Array */ queries = [];
+  if ($filterSearch.value) queries.push(["q", $filterSearch.value]);
+  if ($filterCheckboxes.length) {
+    for (const $checkbox of $filterCheckboxes) {
+      const /**{String} */ key =
+          $checkbox.parentElement.parentElement.dataset.filter;
+      queries.push([key, $checkbox.value]);
+    }
+  }
+  window.location = queries.length
+    ? `?${queries.join("&").replace(/,/g, "=")}`
+    : "/recipes.html";
+});
+
+$filterSearch.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") $filterSubmit.click();
+});
+
+$filterClear.addEventListener("click", function () {
+  const /**{NodeList} */ $filterCheckboxes =
+      $filterBar.querySelectorAll("input:checked");
+  $filterCheckboxes?.forEach((elem) => (elem.checked = false));
+  $filterSearch.value &&= "";
+});
+
+// Clear filter Function
+
+const /**{String} */ queryStr = window.location.search.slice(1);
+const /**{Array} */ queries = queryStr && queryStr.split("&").map(i => i.split("="));
+
+const /**{NodeElement} */ $filterCount = document.querySelector(
+    "[data-filter-count]"
+  );
+
+if (queries.length) {
+  $filterCount.style.display = "block";
+  $filterCount.innerHTML = queries.length;
+} else {
+  $filterCount.style.display = "none";
+}
+
+// Searching API Functionality jo cheez search ho ge woi ahey ge:
+queryStr.split("&").forEach(i => {
+  const [key, value] = i.split("=");
+
+  if (key === "q") {
+    if (value) {
+      $filterBar.querySelector("input[type='search']").value = decodeURIComponent(value.replace(/\+/g, ' '));
+    }
+  } else {
+    if (value) {
+      const element = $filterBar.querySelector(`[value="${decodeURIComponent(value.replace(/\+/g, ' '))}"]`);
+      if (element) {
+        element.checked = true;
+      }
+    }
+  }
+});
+
+// FilterBtn Functionality 
+
+const /**{NodeElement} */ $filterBtn = document.querySelector("[data-filter-btn");
+
+window.addEventListener("scroll" , e =>{
+  $filterBtn.classList[window.scrollY >= 120 ? "add" : "remove"]("active");
+})
+
+// Request Recipe and Render
+
+const /**{NodeElement} */ $gridList =
+    document.querySelector("[data-grid-list]");
+const /**{NodeElement} */ $loadMore =
+    document.querySelector("[data-load-more]");
+
+const /**{Array} */ defaultQueries = [
+    ["mealType", "breakfast"],
+    ["mealType", "dinner"],
+    ["mealType", "lunch"],
+    ["mealType", "snack"],
+    ["mealType", "teatime"],
+  ];
+
+$gridList.innerHTML = $skeletonCard.repeat(20);
+let /**{String} */ nextPageUrl = "";
+
+const renderRecipe = (data) => {
+  data.hits.map((item, index) => {
+    const {
+      recipe: { image, label: title, totalTime: cookingTime, uri },
+    } = item;
+
+    const /**{string} */ recipeId = uri.slice(uri.lastIndexOf("_") + 1);
+    const /**{undefined || String} */ isSaved = window.localStorage.getItem(
+        `cookio-recipe${recipeId}`
+      );
+
+    const /**{NodeElement} */ $card = document.createElement("div");
+    $card.classList.add("card");
+    $card.style.animationDelay = `${100 * index}ms`;
+
+    $card.innerHTML = `
+      
+      <div class="card">
+      <div class="card">
+  <figure class="card-media img-holder">
+    <img
+      src="${image}"
+      width="195"
+      height="195"
+      loading="lazy"
+      alt="${title}"
+      class="img-cover"
+    />
+  </figure>
+  
+  <div class="card-body">
+    <h3 class="title-small">
+      <a href="./detail.html" class="card-link"> ${title ?? "Untitled"}</a>
+    </h3>
+    <div class="meta-wrapper">
+      <div class="meta-item">
+        <span
+          class="material-symbols-outlined"
+          aria-hidden="true"
+          >schedule</span
+        >
+  
+        <span class="label-medium"> ${getTime(cookingTime).time || "<1"} ${
+      getTime(cookingTime).timeUnit
+    } </span>
+      </div>
+      <button
+        class="icon-btn has-state ${isSaved ? "saved" : "removed"}"
+        aria-label="Add to saved recipes" onclick="saveRecipe(this, '${recipeId}')"
+      >
+        <span
+          class="material-symbols-outlined bookmark-add"
+          aria-hidden="true"
+          >bookmark_add</span
+        >
+  
+        <span
+          class="material-symbols-outlined bookmark"
+          aria-hidden="true"
+          >bookmark</span
+        >
+      </button>
+    </div>
+  </div>
+  
+      
+      </div>
+      
+      
+      `;
+
+    $gridList.appendChild($card);
+  });
+};
+
+let /**{Boolean} */ requestedBefore = true;
+fetchData(queries || defaultQueries, (data) => {
+  const {
+    _links: { next },
+  } = data;
+  nextPageUrl = next?.href;
+
+  $gridList.innerHTML = "";
+  requestedBefore = false;
+  if (data.hits.length) {
+    renderRecipe(data);
+  } else {
+    $loadMore.innerHTML = `
+  <p class="body-medium info-text">No Recipe Found</p>
+
+  
+  `;
+  }
+});
+
+// Unlimited data Fetch from API function
+
+const /**{Number} */ CONTAINER_MAX_WIDTH = 1200;
+const /**{Number} */ CONTAINER_MAX_CARD = 6;
+
+window.addEventListener("scroll" , async e => {
+  if($loadMore.getBoundingClientRect().top < window.innerHeight && !requestedBefore && nextPageUrl){
+    $loadMore.innerHTML = $skeletonCard.repeat(Math.round(($loadMore.clientWidth / (CONTAINER_MAX_WIDTH))*CONTAINER_MAX_CARD));
+    requestedBefore=true;
+    
+    const /**{Promise} */ response = await fetch(nextPageUrl);
+    const /**{Object} */ data = await response.json();
+    const {_links: {next}} = data;
+    nextPageUrl = next?.href;
+    renderRecipe(data);
+    $loadMore. innerHTML = "";
+    requestedBefore = false ;
+  }
+
+if(!nextPageUrl) $loadMore.innerHTML = `<p class="body-medium info-text">No More Recipes</p>`;
+
+})
+
+
